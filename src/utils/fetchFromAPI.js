9329fe6b8f8199c5538bb85fa728ca4ce10b7563
chriscutan nadar <chrisrocks2829@gmail.com>
2023-06-27 18:34:31 +0530

    initial

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  url: BASE_URL,
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": "d382c6219dmsh17d3362586704cfp10de91jsnfc58caea0a94",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
